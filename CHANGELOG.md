f5bab63b632351cb1b4f7d5fca3d54b228511e2f
Wade Anderson <waander@Wades-MacBook-Pro.local>
2017-02-27 13:50:02 -0800

    Updated changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,5 @@
 - Added default settings: minimap and format on paste
 - File tree view keyboard shortcuts migrated
 
-## 1.0 
+## 1.0
 - Initial migration of keyboard shortcuts
--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "test" is now active!');
     let config = vscode.workspace.getConfiguration('editor');
     config.update('formatOnPaste', true);
     config.update('minimap.enabled', true);
