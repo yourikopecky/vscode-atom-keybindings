ecbc7121ccfa910590726c2041bc0937a75b5d50
Alex Dima <alexdima@microsoft.com>
2017-03-02 10:01:28 +0100

    Do not write to workspace settings

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    let config = vscode.workspace.getConfiguration('editor');
-    config.update('formatOnPaste', true);
-    config.update('minimap.enabled', true);
+    // let config = vscode.workspace.getConfiguration('editor');
+    // config.update('formatOnPaste', true);
+    // config.update('minimap.enabled', true);
 }
