9c5d1762cda573fa0ec0b5e048a412c44b1b8948
Wade Anderson <waander@Wades-MacBook-Pro.local>
2017-06-21 14:30:42 -0700

    removed extra js files

--- out/src/extension.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-function activate(context) {
-    // let config = vscode.workspace.getConfiguration('editor');
-    // config.update('formatOnPaste', true);
-    // config.update('minimap.enabled', true);
-}
-exports.activate = activate;
-//# sourceMappingURL=extension.js.map
