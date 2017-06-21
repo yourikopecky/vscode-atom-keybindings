79d6d8ad8a8d267bafcb897f744e4ed058bb4e32
Wade Anderson <waander@Wades-MacBook-Pro.local>
2017-06-21 14:30:12 -0700

    new updates for v3

--- src/extension.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import * as vscode from 'vscode';
-
-export function activate(context: vscode.ExtensionContext) {
-    // let config = vscode.workspace.getConfiguration('editor');
-    // config.update('formatOnPaste', true);
-    // config.update('minimap.enabled', true);
-}
